895833a03ba7d53a6ac59ae7b033008b2c04de99
raviNavarro1226 <ravi.navarro@madeinweb.app>
2023-02-15 13:24:07 -0300

    first commit start Project

---
+++ PokeCard-TCA/PokeCard_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeCard_TCAApp.swift
+//  PokeCard-TCA
+//
+//  Created by Ravi navarro on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeCard_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
